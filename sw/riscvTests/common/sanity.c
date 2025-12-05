f9bc952258c3cc48626d59cb62d0fd183ca9ed78
Kai Berszin <kberszin@ethz.ch>
2025-12-05 16:36:51 +0100

    Allow more overwrites for CMake
    Also move sanity tests to cluster level

--- sw/riscvTests/common/sanity.c
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 ETH Zurich and University of Bologna.
-// Solderpad Hardware License, Version 0.51, see LICENSE for details.
-// SPDX-License-Identifier: SHL-0.51
-//
-// Author: Kai Berszin <kberszin@student.ethz.ch>
-
-int main(void) {
-    return 0;
-}
